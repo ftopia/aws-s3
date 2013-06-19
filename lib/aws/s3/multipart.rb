67c7f48b1563a61338f208007d6b3b35487ce368
maukoquiroga <maukoquiroga@gmail.com>
2013-06-19 19:07:39 +0200

    support for listing parts and aborting a multipart operation

--- lib/aws/s3/multipart.rb
+++ lib/aws/s3/multipart.rb
@@ -2,23 +2,23 @@ module AWS
   module S3
     class S3MultipartUpload < Base
       attr_reader :upload_id, :bucket, :key
-      
+
       PartNumberRange = 1..10000
-      
+
       class << self
       end
-      
+
       def initialize(options = {})
         @upload_id  = options[:upload_id]
         @bucket     = options[:bucket] || connection.options[:bucket]
         @key        = options[:key]
-        
+
         super
       end
-      
+
       def upload_part(part_number, options = {})
         raise "part_number is not a number in the 1-10000 range" unless PartNumberRange.include?(part_number)
-        
+
         path = AWS::S3::Connection.prepare_path("#{S3Object.path!(self.key, options)}?partNumber=#{part_number}&uploadId=#{self.upload_id}")
 
         # if no data is given, then generate the necessary url/headers to make the upload
@@ -27,7 +27,7 @@ module AWS
         else
           # generate path and request
           request = Net::HTTP::Put.new(path)
-          
+
           # tuning request
           request['Host']           = "#{self.bucket}.#{DEFAULT_HOST}"
           request['Content-Length'] = options[:size]
@@ -35,30 +35,30 @@ module AWS
                                                                   connection.access_key_id,
                                                                   connection.secret_access_key,
                                                                   :bucket => self.bucket )
-          
+
           # leave the rest to the caller
           return request
         end
-        
+
       end
-      
+
       def complete(etags)
         raise "etags are supposed to be a hash" unless etags.is_a?(Hash)
-        
+
         path  = "#{S3Object.path!(self.key)}?uploadId=#{self.upload_id}"
         xml   = CompleteBuilder.new(etags).to_s
-        
+
         self.class.post self.bucket, path, {}, xml
       end
-      
+
       class CompleteBuilder < XmlGenerator #:nodoc:
         attr_reader :etags
-    
+
         def initialize(etags)
           @etags = etags
           super()
         end
-    
+
         def build
           xml.CompleteMultipartUpload do
             self.etags.keys.sort.each do |part_number|
@@ -70,7 +70,17 @@ module AWS
           end
         end
       end
-      
+
+      def list_parts
+        path = AWS::S3::Connection.prepare_path("#{S3Object.path!(self.key)}?uploadId=#{self.upload_id}")
+        self.class.get self.bucket, path, {}
+      end
+
+      def cancel
+        path = AWS::S3::Connection.prepare_path("#{S3Object.path!(self.key)}?uploadId=#{self.upload_id}")
+        self.class.delete self.bucket, path, {}
+      end
+
     end
   end
 end
